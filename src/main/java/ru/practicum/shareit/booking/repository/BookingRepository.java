9a7ed3ae87b0e18e4065d8480111cd309067cd16
cam1234251 <cam1234251@mail.ru>
2023-07-10 00:08:05 +0300

    Second review

--- src/main/java/ru/practicum/shareit/booking/repository/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/repository/BookingRepository.java
@@ -13,13 +13,13 @@ public interface BookingRepository extends JpaRepository<Booking, Long> {
 
     List<Booking> findByBookerId(Long userId, Pageable page);
 
-    List<Booking> findByBookerIdAndStartBeforeAndEndBefore(Long userId, LocalDateTime time,
-                                                             LocalDateTime t, Pageable page);
+    List<Booking> findByBookerIdAndStartBeforeAndEndBefore(Long userId, LocalDateTime start,
+                                                             LocalDateTime end, Pageable page);
 
     List<Booking> findByBookerIdAndStartAfter(Long userId, LocalDateTime time, Pageable page);
 
-    List<Booking> findByBookerIdAndStartIsBeforeAndEndIsAfter(Long userId, LocalDateTime time,
-                                                               LocalDateTime t, Pageable page);
+    List<Booking> findByBookerIdAndStartIsBeforeAndEndIsAfter(Long userId, LocalDateTime start,
+                                                               LocalDateTime end, Pageable page);
 
     List<Booking> findByBookerIdAndStatus(Long userId, BookingStatus status, Pageable page);
 
@@ -29,8 +29,8 @@ public interface BookingRepository extends JpaRepository<Booking, Long> {
 
     List<Booking> findAllByItem_Owner_IdAndStartIsAfter(Long userId, LocalDateTime time, Pageable page);
 
-    List<Booking> findAllByItem_Owner_IdAndStartIsBeforeAndEndIsAfter(Long userId, LocalDateTime time,
-                                                                      LocalDateTime t, Pageable page);
+    List<Booking> findAllByItem_Owner_IdAndStartIsBeforeAndEndIsAfter(Long userId, LocalDateTime start,
+                                                                      LocalDateTime end, Pageable page);
 
     List<Booking> findAllByItem_Owner_IdAndStatus(Long userId, BookingStatus status, Pageable page);
 
